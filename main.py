62e9bf1b58ea6e8b5d758358b47452d00899515d
Paphon <paphon.jea@mail.kmutt.ac.th>
2022-03-04 16:40:58 +0700

    ไ้ฟะ

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 img = cv2.imread("WIN_20220218_16_09_57_Pro.jpg")
 crop_img = img[0:1072, 390:1540]
-cv2.imshow("cropped", crop_img)
-cv2.waitKey(0)
+cv2.imwrite("WIN_01.jpg",crop_img)
+#cv2.imshow("cropped", crop_img)
+#cv2.waitKey(0)
